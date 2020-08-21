40d297e3891d6b773afb92b300c58184faad01d9
Brian <briansterling93@yahoo.com>
2020-08-21 01:50:33 -0600

    commit

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { MainSection } from "../styling/Home";
 import Navbar from "../components/Navbar";
 import Flair from "../components/Flair";
+import {
+  StateContext,
+  initialState,
+  APP_ACTIONS,
+} from "../context/StateContext";
+import axios from "axios";
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    populateItems();
+  }, []);
+  const { state, dispatch } = useContext<any>(StateContext);
+  const [items, updateItems] = useState<[]>();
+
+  const populateItems = async () => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await axios.get("/admin");
+
+      const res2 = res.data.findAll;
+
+      console.log(res2);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Flair />
