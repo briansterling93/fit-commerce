22e9ebc425f86085844c1e2be8d5c7c332145f15
Brian <briansterling93@yahoo.com>
2020-12-17 22:49:27 -0700

    commit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`App running on Port ${PORT}`);
