47bee6280e7b414cb720be96cbca19d721398832
Brian <briansterling93@yahoo.com>
2020-12-20 20:06:30 -0700

    commit

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ app.use("/cart", require("./routes/cart.js"));
 app.use("/user_carts", require("./routes/user_carts.js"));
 app.use("/orders", require("./routes/order_history.js"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
